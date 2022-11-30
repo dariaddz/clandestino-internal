8640cef128d176d12ec9ce490b0594bfa8fb66b4
dariaddz <dariadz@gmail.com>
2022-11-30 16:40:09 +0300

    First commit

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+export default function MainPage() {
+  return (
+    <>
+      <h1>Bloco Clandestino</h1>
+      <h2>internal</h2>
+
+      <div>Search</div>
+      <nav>navigation</nav>
+      <div>music list</div>
+    </>
+  );
+}
