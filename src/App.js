d0021670713cae51ab2d1d4be20a8d3a182f5e4b
dariaddz <dariadz@gmail.com>
2022-12-04 23:15:39 +0300

    loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import MainPage from './pages/MainPage';
 
 import LoginPage from './pages/LoginPage';
+import Loader from './components/Loader';
+
 import { Container } from './fonts-colors/styledComponents';
 
 function App() {
   return (
-    <Container
-    // style={{
-    //   backgroundImage: 'url(' + img + ')',
-    //   backgroundPosition: 'center',
-    //   backgroundSize: 'cover',
-    //   backgroundRepeat: 'no-repeat',
-    //   height: '100vh',
-    //   width: '100vw',
-    // }}
-    >
-      <MainPage />
-      <LoginPage />
-    </Container>
+    <>
+      <Loader />
+      <Container
+      // style={{
+      //   backgroundImage: 'url(' + img + ')',
+      //   backgroundPosition: 'center',
+      //   backgroundSize: 'cover',
+      //   backgroundRepeat: 'no-repeat',
+      //   height: '100vh',
+      //   width: '100vw',
+      // }}
+      >
+        <MainPage />
+        <LoginPage />
+      </Container>
+    </>
   );
 }
 
